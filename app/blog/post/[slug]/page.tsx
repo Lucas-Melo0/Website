8b26d413525136a9b93edc21a46e8cdd54c0e4d0
Lucas-Melo0 <lucassss8v@gmail.com>
2023-01-09 19:20:03 -0300

    feat:Centralized text on individual post page

--- app/blog/post/[slug]/page.tsx
+++ app/blog/post/[slug]/page.tsx
@@ -4,11 +4,13 @@ import { getSinglePost } from '../../../../services/notion';
 export default async function Post({ params: post }: { params: { slug: string } }) {
   const postData = await getSinglePost(post.slug);
 
-  console.log(postData);
-  //@ts-ignore
   return (
-    <article className="prose prose-invert prose-lg max-w-none">
-      <ReactMarkdown remarkPlugins={[remarkGfm]}>{postData.markdown}</ReactMarkdown>;
-    </article>
+    <div className="min-h-screen">
+      <div className="w-5/12 mx-auto gap-y-3   mt-20 flex flex-col items-start">
+        <article className="prose prose-invert prose-lg max-w-none">
+          <ReactMarkdown remarkPlugins={[remarkGfm]}>{postData.markdown}</ReactMarkdown>;
+        </article>
+      </div>
+    </div>
   );
 }
