fda633d2227433a25906c7645f189af75c178ff2
Lucas-Melo0 <lucassss8v@gmail.com>
2023-02-20 11:03:32 -0300

    feat: add a timeline experience to the about me page

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -7,7 +7,7 @@ export default async function Page() {
   const posts = await getAllPublished();
   const results: any = posts.results;
   return (
-    <div className="min-h-screen bg-gray-900 dark:bg-white ">
+    <div className="min-h-screen bg-white dark:bg-gray-900 ">
       <div className="w-5/12 mx-auto gap-y-3 flex flex-col items-start">
         <div className="mt-16 gap-y-3 flex flex-col">
           {results.map((post: any) => {
@@ -15,23 +15,23 @@ export default async function Page() {
               <Link href={`blog/post/${post.properties.Slug.rich_text[0].plain_text}`}>
                 <div className="  cursor-pointer ">
                   <div>
-                    <h1 className="text-2xl text-white hover:text-orange-400 dark:text-black dark:hover:text-orange-400 font-bold">
+                    <h1 className="text-2xl hover:text-orange-400 dark:text-white dark:hover:text-orange-400 font-bold">
                       {post.properties.Name.title[0].plain_text}{' '}
                     </h1>
                   </div>
-                  <div className="text-gray-400 dark:text-gray-800">
+                  <div className="text-gray-800 dark:text-gray-400">
                     {post.properties.Description.rich_text[0].plain_text}
                   </div>
                   <div className="flex gap-x-4 flex-row">
                     <div className="flex items-center gap-x-2">
                       <AiOutlineCalendar color="gray" />
-                      <p className="text-gray-400 dark:text-gray-800">
+                      <p className="text-gray-800 dark:text-gray-400">
                         {DateFormatter(post.properties.Date.date.start)}
                       </p>
                     </div>
                     <div className="flex items-center gap-x-2">
                       <AiOutlineUser color="gray" />
-                      <p className="text-gray-400 dark:text-gray-800">Lucas Melo</p>
+                      <p className="text-gray-800 dark:text-gray-400">Lucas Melo</p>
                     </div>
                   </div>
                   <span className="w-full border-gray-600 border-b"></span>
